432d5e1242767157c3df5089bbeffd8055198be4
Ian Fijolek <ian@iamthefij.com>
2017-12-22 19:01:45 -0500

    Add empty check

--- encryption-check.sh
+++ encryption-check.sh
@@ -5,8 +5,12 @@ set -e
 has_error=0
 for file in $@ ; do
     head -1 "$file" | grep --quiet '^\$ANSIBLE_VAULT;' || {
-        echo "ERROR: $file is not encrypted"
-        has_error=1
+        if [ -s "$file" ]; then
+            echo "ERROR: $file is not encrypted"
+            has_error=1
+        else
+            echo "WARNING: $file is not encrypted but is empty"
+        fi
     }
 done
 
